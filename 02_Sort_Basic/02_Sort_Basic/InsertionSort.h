c6052a49ae27fd9885bb9a1d87c28ce34f94368e
Andy <smrtwater@126.com>
2018-03-20 23:05:19 +0800

    排序____插入排序

---
+++ 02_Sort_Basic/02_Sort_Basic/InsertionSort.h
@@ -0,0 +1,45 @@
+//
+//  InsertionSort.h
+//  02_Sort_Basic
+//
+//  Created by yhp on 2018/3/20.
+//  Copyright © 2018年 none. All rights reserved.
+//
+
+#ifndef InsertionSort_h
+#define InsertionSort_h
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+void InsertionSort(T arr[],int n){
+    for (int i = 1; i < n; ++i) {
+        for (int j = i; j > 0; --j) {
+            if (arr[j] < arr[j-1]) {
+                swap(arr[j], arr[j-1]);
+            }else{
+                break;
+            }
+        }
+    }
+}
+
+template <typename T>
+void InsertionSortAdvance(T arr[],int n){
+    for (int i = 1; i < n; ++i) {
+        T e = arr[i];
+        int j;
+        for (j = i; j > 0; --j) {
+            if (arr[j-1] > e) {
+                arr[j] = arr[j-1];
+            }else{
+                break;
+            }
+        }
+        arr[j] = e;
+    }
+}
+
+#endif /* InsertSort_h */
--- 02_Sort_Basic/02_Sort_Basic/main.cpp
+++ 02_Sort_Basic/02_Sort_Basic/main.cpp
@@ -10,13 +10,24 @@
 #include <algorithm>
 #include "SortTestHelper.h"
 #include "SlectionSort.h"
+#include "InsertionSort.h"
 
+
+
+/**
+ 排序算法总结:
+ o(n*n):
+ 优化后插入排序的算法效率要高于选择排序
+ */
 int main(int argc, const char * argv[]) {
 
     int n = 10000;
     
     int* arr = SortTestHelper::generateRandomArray(n, 0, n);
-    SortTestHelper::testSort("slectionSort", selectionSort, arr, n);
+//    SortTestHelper::printArray(arr, n);
+//    SortTestHelper::testSort("slectionSort", selectionSort, arr, n);
+    SortTestHelper::testSort("slectionSort", InsertionSortAdvance, arr, n);
+//    SortTestHelper::printArray(arr, n);
     delete [] arr;
     
     return 0;
