88613c4a72b524dc8ae7e579556052dd4e19bb53
Andy <smrtwater@126.com>
2018-03-25 23:43:53 +0800

    排序___归并排序

---
+++ 03_Sort_ Advance/03_Sort_ Advance/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  03_Sort_ Advance
+//
+//  Created by yhp on 2018/3/25.
+//  Copyright © 2018年 none. All rights reserved.
+//
+
+#include <iostream>
+#include "SortTestHelper.h"
+#include "MergeSort.h"
+
+int main(int argc, const char * argv[]) {
+    
+    int n = 100000;
+    
+    int* arr = SortTestHelper::generateRandomArray(n, 0, n);
+//        SortTestHelper::printArray(arr, n);
+    SortTestHelper::testSort("mergeSort", mergeSort, arr, n);
+//        SortTestHelper::printArray(arr, n);
+    delete [] arr;
+    
+    return 0;
+}
