5713cb93bbc0a7e2ce8230dbb745b14971dcd8aa
Andy <smrtwater@126.com>
2018-04-22 21:54:53 +0800

    添加最小堆实现及最小堆测试代码

--- 04_Heap/04_Heap/Heap.h
+++ 04_Heap/04_Heap/Heap.h
@@ -121,4 +121,81 @@ public:
 };
 
 
+template <typename Item>
+class MinHeap {
+    Item* data;
+    int   count;
+    int   capacity;
+//    function
+    void shiftUp(int k){
+        while (k > 1 && data[k / 2] > data[k]) {
+            swap(data[k / 2], data[k]);
+            k /= 2;
+        }
+    }
+    void shiftDown(int k){
+        while (2 * k <= count) {
+            int j = 2 * k;
+//           判断是往左还是右子树shiftdown
+            if ( j + 1 <= count && data[j + 1] < data[j]) {
+                j ++;
+            }
+            if (data[k] <= data[j]) {
+                break;
+            }
+            swap(data[k], data[j]);
+            k = j;
+        }
+    }
+public:
+    MinHeap(int capacity){
+        data  = new Item[capacity +1];
+        count = 0;
+        this->capacity = capacity;
+    }
+    ~MinHeap(){
+        delete [] data;
+    }
+    int size(){
+        return count;
+    }
+    
+    bool isEmpty(){
+        return count == 0;
+    }
+    
+    void insert(Item item){
+        assert(count + 1 <= capacity);
+        data[count + 1] = item;
+        shiftUp(count + 1);
+        count++;
+    }
+    
+    Item extractMin(){
+        assert(count > 0);
+        Item ret = data[1];
+//      从数组末尾copy一个元素到数组1的位置
+        swap(data[1], data[count]);
+        count--;
+        shiftDown(1);
+        return ret;
+    }
+    
+    Item getMin(){
+        assert(count > 0);
+        return data[1];
+    }
+    
+    void show(){
+        count << "| ";
+        for (int i = 1; i <= count; ++i) {
+            cout << data[i] << " | ";
+        }
+        cout << endl;
+    }
+    
+    
+};
+
+
 #endif /* Heap_h */
--- 04_Heap/04_Heap/main.cpp
+++ 04_Heap/04_Heap/main.cpp
@@ -11,5 +11,19 @@
 
 int main(int argc, const char * argv[]) {
     
+    int a[10] = {9,10,11,8,1,3,6,5,4,7};
+    MinHeap<int>* heap = new MinHeap<int>(10);
+    
+    for (int i = 0; i < 10; i++) {
+        heap->insert(a[i]);
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        printf("%d ",heap->extractMin());
+    }
+    printf("\n");
+    
+    delete heap;
+    
     return 0;
 }
